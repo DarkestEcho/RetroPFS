6d6fc59ef26ff7ee972d929905e0ccb96e9e7803
Dmitriy Samarin <Samarin99@gmail.com>
2026-04-20 17:49:50 +0300

    Added "RFPSBulletWeapon" class files

---
+++ Source/RetroFPS/Private/Weapons/RFPSBulletWeapon.cpp
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapons/RFPSBulletWeapon.h"
+#include "Utils/RFPSUtils.h"
+#include "Types/RFPSTypes.h"
+
+#include "PaperFlipbookComponent.h"
+
+
+// Sets default values
+ARFPSBulletWeapon::ARFPSBulletWeapon()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void ARFPSBulletWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+void ARFPSBulletWeapon::StartFire()
+{
+	if ( bFiring )
+	{
+		return;
+	}
+
+	const FHitResult HitResult = RFPSUtils::MakeLineTraceSingle(
+		GetWorld(),
+		{
+			RFPSUtils::GetPawnViewPoint( Cast<APawn>( GetOwner() ) ),
+			Distance,
+			ECC_Visibility,
+			{ this, GetOwner() }
+		}
+	);
+
+	DrawDebugLine(
+		GetWorld(),
+		FireStartPoint->GetComponentLocation(),
+		HitResult.bBlockingHit ? HitResult.ImpactPoint : HitResult.TraceEnd,
+		HitResult.bBlockingHit ? FColor::Red : FColor::Green,
+		false,
+		10.0f,
+		0,
+		1.0f
+	);
+
+	if ( HitResult.bBlockingHit )
+	{
+		DrawDebugSphere(
+			GetWorld(),
+			HitResult.ImpactPoint,
+			10.0f,
+			8,
+			FColor::Red,
+			false,
+			10.0f,
+			0,
+			1.0f
+		);
+	}
+
+	FlipbookComponent->SetFlipbook( FlipbookFire );
+	FlipbookComponent->SetLooping( false );
+	FlipbookComponent->PlayFromStart();
+	FlipbookComponent->OnFinishedPlaying.Clear();
+	FlipbookComponent->OnFinishedPlaying.AddDynamic( this, &ARFPSBulletWeapon::OnFireAnimationFinished );
+
+	bFiring = true;
+
+	// TODO+: Prepare Data For Trace
+	// TODO+: Make Trace
+	// TODO+: Check Trace Result
+	// TODO: Apply Damage
+}
+
+void ARFPSBulletWeapon::StopFire()
+{
+}
+
+void ARFPSBulletWeapon::OnFireAnimationFinished()
+{
+	FlipbookComponent->SetFlipbook( FlipbookIdle );
+	FlipbookComponent->PlayFromStart();
+	FlipbookComponent->OnFinishedPlaying.Clear();
+	bFiring = false;
+}
---
+++ Source/RetroFPS/Public/Weapons/RFPSBulletWeapon.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RFPSBaseWeapon.h"
+#include "RFPSBulletWeapon.generated.h"
+
+UCLASS( Blueprintable )
+class RETROFPS_API ARFPSBulletWeapon : public ARFPSBaseWeapon
+{
+	GENERATED_BODY()
+
+public:
+	ARFPSBulletWeapon();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void StartFire() override;
+	virtual void StopFire() override;
+	
+protected:
+	UFUNCTION()
+	void OnFireAnimationFinished();
+};
