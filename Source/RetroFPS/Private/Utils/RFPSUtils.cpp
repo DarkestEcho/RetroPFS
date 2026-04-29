8c4db9faac94db9c9ad66bb8ba2b1b65da264935
Dmitriy Samarin <Samarin99@gmail.com>
2026-04-29 19:55:40 +0300

    Added new utils

--- Source/RetroFPS/Private/Utils/RFPSUtils.cpp
+++ Source/RetroFPS/Private/Utils/RFPSUtils.cpp
@@ -1,5 +1,6 @@
 #include "Utils/RFPSUtils.h"
 
+#include "Components/TextBlock.h"
 #include "GameFramework/Actor.h"
 #include "GameFramework/Controller.h"
 
@@ -17,8 +18,19 @@ FViewPoint RFPSUtils::GetPawnViewPoint( const APawn* Pawn )
 	return ViewPoint;
 }
 
+FVector RFPSUtils::GetLineTraceDirection( const FViewPoint& ViewPoint, const float SpreadAngleHorizontal, const float SpreadAngleVertical )
+{
+	if ( SpreadAngleHorizontal > 0.0f || SpreadAngleVertical > 0.0f )
+	{
+		const float HalfRadiansHorizontal = FMath::DegreesToRadians( SpreadAngleHorizontal ) / 2.0f;
+		const float HalfRadiansVertical = FMath::DegreesToRadians( SpreadAngleVertical ) / 2.0f;
+		return FMath::VRandCone( ViewPoint.Rotation.Vector(), HalfRadiansHorizontal, HalfRadiansVertical );
+	}
+	return ViewPoint.Rotation.Vector();
+}
 
-FHitResult RFPSUtils::MakeLineTraceSingle( 	const UWorld* World, const FTraceData& TraceData )
+
+FHitResult RFPSUtils::MakeLineTraceSingle( const UWorld* World, const FTraceData& TraceData )
 {
 	FHitResult HitResult {};
 	if ( IsValid( World ) )
@@ -27,7 +39,8 @@ FHitResult RFPSUtils::MakeLineTraceSingle( 	const UWorld* World, const FTraceDat
 		CollisionParams.AddIgnoredActors( TraceData.IgnoredActors );
 
 		const FVector TraceStart { TraceData.ViewPoint.Location };
-		const FVector TraceEnd { TraceData.ViewPoint.Location + TraceData.ViewPoint.Rotation.Vector() * TraceData.Distance };
+		const FVector TraceDirection { GetLineTraceDirection( TraceData.ViewPoint, TraceData.SpreadAngleHorizontal, TraceData.SpreadAngleVertical ) };
+		const FVector TraceEnd { TraceData.ViewPoint.Location + TraceDirection * TraceData.Distance };
 
 		World->LineTraceSingleByChannel(
 			HitResult,
@@ -36,7 +49,62 @@ FHitResult RFPSUtils::MakeLineTraceSingle( 	const UWorld* World, const FTraceDat
 			TraceData.CollisionChannel,
 			CollisionParams
 		);
+
+		if ( TraceData.bShowDebugLine )
+		{
+			DrawDebugHitTrace( World, HitResult, TraceData.DebugLineStart );
+		}
 	}
 
 	return HitResult;
+}
+
+FText RFPSUtils::GetTextFromInt( const int32 Number )
+{
+	return FText::FromString( FString::FromInt( Number ) );
+}
+
+void RFPSUtils::SetAmmoDataTextBlocks( UTextBlock* CurrentAmmo, UTextBlock* MaxAmmo, const FAmmoData AmmoData )
+{
+	if ( !IsValid( CurrentAmmo ) || !IsValid( MaxAmmo ) )
+	{
+		return;
+	}
+
+	CurrentAmmo->SetText( RFPSUtils::GetTextFromInt( AmmoData.CurrentAmmo ) );
+	MaxAmmo->SetText( RFPSUtils::GetTextFromInt( AmmoData.MaxAmmo ) );
+}
+
+void RFPSUtils::DrawDebugHitTrace( const UWorld* World, const FHitResult& HitResult, const FVector& LineStart )
+{
+	if ( !IsValid( World ) )
+	{
+		return;
+	}
+
+	DrawDebugLine(
+		World,
+		LineStart,
+		HitResult.bBlockingHit ? HitResult.ImpactPoint : HitResult.TraceEnd,
+		HitResult.bBlockingHit ? FColor::Red : FColor::Green,
+		false,
+		10.0f,
+		0,
+		1.0f
+	);
+
+	if ( HitResult.bBlockingHit )
+	{
+		DrawDebugSphere(
+			World,
+			HitResult.ImpactPoint,
+			10.0f,
+			8,
+			FColor::Red,
+			false,
+			10.0f,
+			0,
+			1.0f
+		);
+	}
 }
--- Source/RetroFPS/Public/Utils/RFPSUtils.h
+++ Source/RetroFPS/Public/Utils/RFPSUtils.h
@@ -1,11 +1,29 @@
 ﻿#pragma once
 #include "Types/RFPSTypes.h"
 
+class UTextBlock;
 class ACharacter;
 
 class RFPSUtils
 {
 public:
 	static FViewPoint GetPawnViewPoint( const APawn* Pawn );
+	static FVector GetLineTraceDirection( const FViewPoint& ViewPoint, const float SpreadAngleHorizontal, const float SpreadAngleVertical );
 	static FHitResult MakeLineTraceSingle( const UWorld* World, const FTraceData& TraceData );
+	static FText GetTextFromInt( int32 Number );
+	static void SetAmmoDataTextBlocks( UTextBlock* CurrentAmmo, UTextBlock* MaxAmmo, FAmmoData AmmoData );
+	static void DrawDebugHitTrace( const UWorld* World, const FHitResult& HitResult, const FVector& LineStart );
+
+	template <class T>
+	static T* GetComponentFromActor( const AActor* Actor );
 };
+
+template <class T>
+T* RFPSUtils::GetComponentFromActor( const AActor* Actor )
+{
+	if ( !Actor )
+	{
+		return nullptr;
+	}
+	return Cast<T>( Actor->GetComponentByClass( T::StaticClass() ) );
+}
